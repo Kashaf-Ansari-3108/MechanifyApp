a4a9b5c320eff60a6ff8f4fc2b0c8958223771c6
Kashaf Ansari <63552980+Kashaf-Ansari-3108@users.noreply.github.com>
2024-02-27 16:39:36 -1200

    .

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://dull-gray-sari.cyclic.app/api/'
+    baseURL: 'https://odd-jade-brown-bear-hem.cyclic.app/api/'
     
 })
 
